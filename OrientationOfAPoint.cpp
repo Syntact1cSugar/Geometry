4c825c764b2eaad6397e9cc8384562fc5e7ff070
Syntact1cSugar <121145814+Syntact1cSugar@users.noreply.github.com>
2023-01-10 14:04:10 +0530

    Add files via upload

---
+++ OrientationOfAPoint.cpp
@@ -0,0 +1,10 @@
+#include "BasicGeometry.h"
+#include <string>
+// Returns the orientation of a point p(x, y) with respect to point B that lies on the line
+// segment AB
+
+std::string pointOrientation(const point2D& A, const point2D& B, const point2D& P)
+{
+
+	return (cross(A-B, A-P) > 0)? "ANTICLOCKWISE" : "CLOCKWISE";
+}
